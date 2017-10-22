3c1dde5de627264b88e09bdb103613a67e3bf9d0
zzeleznick <zzeleznick@gmail.com>
2017-10-21 22:18:39 -0700

    fix silly mistake

--- dist.py
+++ dist.py
@@ -16,37 +16,28 @@ def eval_ours():
     labeled_preds = predict_dist(121)
     eval_preds(labeled_preds, "TRACK_DISTANCE", "dist", "OURS")
 
-def eval_actigraph():
-    tr = load_train()
-    labeled_preds = tr[['PID', 'ACTIGRAPH_STEPS']]
-    labeled_preds.columns = ['PID', 'PRED']
-    eval_preds(labeled_preds, "TRACK_DISTANCE", "dist", "ACTIGRAPH")
-
 def eval_vastrac():
     tr = load_train()
-    labeled_preds = tr[['PID', 'VASCTRAC_STEPS']]
+    labeled_preds = tr[['PID', 'VASCTRAC_DISTANCE']]
     labeled_preds.columns = ['PID', 'PRED']
     eval_preds(labeled_preds, "TRACK_DISTANCE", "dist", "VASCTRAC")
 
 def eval_naive():
     tr = load_train()
-    labeled_preds = tr[['PID', 'VASCTRAC_STEPS']]
-    labeled_preds['VASCTRAC_STEPS'] = 540 * np.ones(len(tr))
+    labeled_preds = tr[['PID', 'VASCTRAC_DISTANCE']]
+    labeled_preds['VASCTRAC_DISTANCE'] = 1000 * np.ones(len(tr))
     labeled_preds.columns = ['PID', 'PRED']
     eval_preds(labeled_preds, "TRACK_DISTANCE", "dist", "NAIVE")
 
 def main():
     parser = argparse.ArgumentParser(description="Main")
     group = parser.add_mutually_exclusive_group()
-    group.add_argument('-a', '--actigraph', action='store_true')
     group.add_argument('-v', '--vastrac', action='store_true')
     group.add_argument('-n', '--naive', action='store_true')
     group.add_argument('-o', '--ours', action='store_true')
     args = parser.parse_args()
     if args.vastrac:
         eval_vastrac()
-    elif args.actigraph:
-        eval_actigraph()
     elif args.naive:
         eval_naive()
     else:
